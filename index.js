7a43db31e44f605ab845c2dba423c0cf8811fe29
Justin Pradier <chromecide@chromecide.com>
2012-10-09 23:58:30 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var mixinFunctions = {
+	init: function(){
+		var thisNode = this;
+		
+		
+		//add properties that are needed by this mixin
+	
+		//add Events that are emitted by this mixin
+		
+		
+		//if it isn't already, we need to mixin the FNMGroupSecurity
+		if(!thisNode.FNMGroupSecurity_Settings){
+			thisNode.mixin('FNMGroupSecurity', {}, function(){
+				console.log('Mixed In');
+			});	
+		}
+		
+		thisNode.TunnelManager.configureManager({
+			allowed: function(action, tunnel, topic, message){
+				if(!tunnel.isLoggedIn){
+					tunnel.isLoggedIn = false;
+				}
+				var isAllowed = true;
+				if(action=='recieve'){
+					switch(topic){
+						case 'init':
+							//leave well enough alonel
+							break;
+						case 'Security.Login': //the other end is attempting to login
+							return true;
+							break;
+						case 'Security.Logout': //the other end is attempting to login
+							return true;
+							break;
+						default:
+							if(!tunnel.isLoggedIn){
+								thisNode.TunnelManager.send(tunnel, 'MustLogin', {message: 'This server requires authentication, please log in.'})
+							}
+							return tunnel.isLoggedIn;
+							break;
+					}
+				}
+				return isAllowed;
+			}
+		});
+		
+		thisNode.on('Security.Login', function(message, rawMessage){
+			console.log('Security Auth Recieved');
+			console.log(message);
+			
+			thisNode.StorageManager.findOne(message, thisNode.FNMGroupSecurity_Settings.store, thisNode.FNMGroupSecurity_Settings.userChannel, function(err, recs){
+				if(!err){
+					if(recs.length>0){
+						thisNode.TunnelManager.getTunnel(rawMessage._message.sender).isLoggedIn = true;
+						thisNode.sendEvent(rawMessage._message.sender, 'Security.LoginSuccessful', {name: recs[0].name});
+					}else{
+						thisNode.sendEvent(rawMessage._message.sender, 'Security.LoginFailed', {});
+					}
+				}
+			});
+		});
+	}
+}
+
+if (typeof define === 'function' && define.amd) {
+	define(mixinFunctions);
+} else {
+	module.exports = mixinFunctions;
+}
+	
